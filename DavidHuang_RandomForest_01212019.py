98b3c24075d683876c4e415f0d0e8b8e87217fd4
David Huang <42119351+hiydavid@users.noreply.github.com>
2019-01-21 21:36:33 -0800

    Create DavidHuang_RandomForest_01212019.py

---
+++ DavidHuang_RandomForest_01212019.py
@@ -0,0 +1,105 @@
+### RANDOM FOREST MODELING
+### VERSION: df_features_1
+
+############################################################ LOAD & PREP
+
+# Load packages
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Set options
+pd.set_option('display.max_rows', 100)
+pd.set_option('display.max_columns', 100)
+pd.set_option('display.float_format', '{:,.5f}'.format)
+
+# Load datasets
+data = pd.read_csv('df_features_1.csv')
+
+# Setting random state
+SEED = 1234
+
+############################################################ WORKFLOW FUNCTIONS
+
+# Load packages
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error as MSE
+
+# Review city dataframes
+# LA = data[data['City'] == "LA"].dropna(axis = 'rows')
+# NY = data[data['City'] == "NYC"].dropna(axis = 'rows')
+# DC = data[data['City'] == "DC"].dropna(axis = 'rows')
+    
+# Create data frame cleaning funtion
+def rando(df, train_city, y_var, n_trees, depth):
+    
+    # Split data into training and test sets
+    train = data[data['City'] == train_city].dropna(axis = 'rows')
+    test = data[data['City'] == "DC"].dropna(axis = 'rows')
+    
+    # Split data into 
+    y_train = train[y_var]
+    y_test = test[y_var]
+    X_train = train.drop(['GEOID', 'City', 'Collisions', 'TotalInjuries', 'TotalDeaths'], axis = 1)
+    X_test = test.drop(['GEOID', 'City', 'Collisions', 'TotalInjuries', 'TotalDeaths'], axis = 1)
+    
+    # Model fitting
+    rf = RandomForestRegressor(
+        n_estimators = n_trees, 
+        max_depth = depth,
+        random_state = SEED)
+    rf.fit(X_train, y_train)
+    
+    # Predict class probability using fitted model 
+    y_pred = rf.predict(X_test)
+    
+    # Score calculation
+    fit_score = rf.score(X_train, y_train)
+    rmse_score = np.sqrt(MSE(y_test, y_pred))
+    print("Model Fit: {:.6f}".format(fit_score))
+    print("Model RMSE: {:.2f}".format(rmse_score))
+    print("y-Test Mean: {:.2f}".format(np.mean(y_test)))
+    print("y-Test Stdev: {:.2f}".format(np.std(y_test)))
+    
+    # Visualizing features importances
+    importances = pd.Series(
+    	data = rf.feature_importances_,
+    	index = X_train.columns)
+    importances_sorted = importances.sort_values()
+    importances_sorted.plot(kind = 'barh', color = 'lightgreen')
+    plt.title('Features Importances')
+    plt.show()
+
+############################################################ NYC: TOTAL INJURIES
+
+# Run random forest model on NYC, Total Injuries
+rando(df = data, 
+      train_city = "NYC", 
+      y_var = "TotalInjuries", 
+      n_trees = 100, 
+      depth = 3
+      )
+
+# Run random forest model on NYC, Total Deaths
+rando(df = data, 
+      train_city = "NYC", 
+      y_var = "TotalDeaths", 
+      n_trees = 100, 
+      depth = 3
+      )
+
+# Run random forest model on LA, Total Injuries
+rando(df = data, 
+      train_city = "LA", 
+      y_var = "TotalInjuries", 
+      n_trees = 100, 
+      depth = 3
+      )
+
+# Run random forest model on LA, Total Deaths
+rando(df = data, 
+      train_city = "LA", 
+      y_var = "TotalDeaths", 
+      n_trees = 100, 
+      depth = 3
+      )
