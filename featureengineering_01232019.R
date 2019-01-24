5c7405876460d8799dc5ad1a3fe46e7295d409d8
David Huang <42119351+hiydavid@users.noreply.github.com>
2019-01-23 19:47:19 -0800

    Update to add pedestrian data

--- featureengineering_01232019.R
+++ featureengineering_01232019.R
@@ -1,6 +1,12 @@
 # David Huang
 # Capstone Modeling Preparation & Feature Engineering
 
+##################################################  Change Log
+# 2019-01-21  Created file
+# 2019-01-23  Updated to include pedestrian data
+
+
+
 ##################################################  Load libraries and data 
 library(readr)
 library(tidyr)
@@ -10,9 +16,11 @@ library(lubridate)
 library(scales)
 library(tidycensus)
 
+
+
 ################################################## Read and transform master data
 
-# Set directory
+# Set directory (change to your own directory)
 setwd("D:/_dhuang/Work/NYU Stern MSBA Work/Capstone/Data/CapstoneModeling")
 
 # Upload old master data
@@ -23,10 +31,12 @@ data$GEOID <- as.character(data$GEOID)
 census <- data %>% select(26, 37:96)
 census <- unique(census)
 
-# Aggregate the collisions, injuries and deaths by city and census tracts
+# Aggregate by city and census tracts
 collisions <- data %>%
   group_by(City, GEOID) %>%
-  summarise(Collisions = n(), 
+  summarise(Collisions = n(),
+            PedeInjuries = sum(TotalPedestrianInjuries),
+            PedeDeaths = sum(TotalPedestrianDeaths),
             TotalInjuries = sum(TotalInjuries), 
             TotalDeaths = sum(TotalDeaths))
 
@@ -34,9 +44,11 @@ collisions <- data %>%
 write.csv(collisions, "data_collisions.csv", row.names = FALSE)
 write.csv(census, "data_census.csv", row.names = FALSE)
 
+
+
 ################################################## Merge, transform, and clean features
 
-# Bring in the tract square mile data
+# Bring in the tract squarefootage data
 area <- read_csv("data_2010_area.csv")
 area <- area[,-(2:5)]
 colnames(area) <- c("GEOID", "sqmi_total", "sqmi_water", "sqmi_land")
@@ -67,6 +79,8 @@ view_corr <- function(df) {
 # Review feature_0
 view_corr(features_0)
 
+
+
 ################################################## Merge, transform, and clean features
 
 ############################## Iteration 1
@@ -74,8 +88,7 @@ features_1 <- features_0 %>%
   group_by(GEOID) %>%
   transmute(pop_dens = (pop / (sqmi_land)),
             race_white,
-            race_minority = (sum(race_black, race_asian, race_hispanic, race_native, 
-                                 race_hawaiian, race_other, race_twoplus)),
+            race_minority = (sum(race_black, race_asian, race_hispanic, race_native, race_hawaiian, race_other, race_twoplus)),
             female,
             age_genz = (sum(age_under5, age_5t17)),
             age_millenial = (sum(age_18t24, age_25t34)),
@@ -105,17 +118,15 @@ df_features_1 <- left_join(features_1, collisions, by = "GEOID")
 View(df_features_1)
 write.csv(df_features_1, "df_features_1.csv", row.names = FALSE)
 
-# Note: This iteration was mostly combining and removing variables without any 
-#       normalization or transformation, with the exception of normalizing 
-#       population by land square miles.
-
+# Note: This iteration was mostly combining and removing variables
+#       without any normalization or transformation, with the exception
+#       of normalizing population by land square miles.
 
 ############################## Iteration 2
 features_2 <- features_0 %>%
   group_by(GEOID) %>%
   transmute(pop_dens = (pop / (sqmi_land)),
-            perc_minority = (sum(race_black, race_asian, race_hispanic, race_native, 
-                                 race_hawaiian, race_other, race_twoplus) / pop),
+            perc_minority = (sum(race_black, race_asian, race_hispanic, race_native, race_hawaiian, race_other, race_twoplus) / pop),
             perc_female = (female / pop),
             perc_genz = (sum(age_under5, age_5t17) / pop),
             perc_millenial = (sum(age_18t24, age_25t34) / pop),
@@ -145,10 +156,9 @@ df_features_2 <- left_join(features_2, collisions, by = "GEOID")
 View(df_features_2)
 write.csv(df_features_2, "df_features_2.csv", row.names = FALSE)
 
-# Note: This iteration picks up from iteration 1, with the addition of 
-#       normalization by population where applicable. No transformations 
+# Note: This iteration picks up from iteration 1, with the addition of
+#       normalization by population where applicable. No transformations
 #       were done otherwise.
 
-
 ############################## Iteration 3
-
+# TO BE CONTINUED...
