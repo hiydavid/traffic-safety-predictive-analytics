5fd1f4a8de404ecfe3a8f17431b76db8583ce746
David Huang <hiydavid@gmail.com>
2019-02-02 22:52:19 -0800

    Seperated NY from LA

--- RandomForest_NY.py
+++ RandomForest_NY.py
@@ -1,6 +1,6 @@
 ### Owner:      David Huang
 ### Model:      Random Forest
-### City:       Los Angeles
+### City:       New York City
 ### Date:       2019-02-02
 
 ############################################################ CHANGE LOG
@@ -30,6 +30,7 @@ pd.set_option('display.float_format', '{:,.5f}'.format)
 f1 = pd.read_csv('df_features_1.csv')       # Count data
 f2 = pd.read_csv('df_features_2.csv')       # Normalized by population
 f3 = pd.read_csv('df_features_3.csv')       # Normalized by land
+f4 = pd.read_csv('df_features_4.csv')       # Ron's new variables
 
 # Setting random state
 SEED = 1234
@@ -96,31 +97,55 @@ def rando(df, city, y_var, n_trees, depth, max_feat):
     print("END OF OUTPUT ***************************************")
     print("")
 
-############################################################ LA, OVERALL CASUALTIES
+############################################################ NYC, OVERALL CASUALTIES
+    
+# NYC Casualties Mean: 110.38
+# NYC Casualtes Stdev: 95.72
 
-# Run random forest model on LA, Total Injuries, Feature Set 1
+# Run random forest model on NYC, Total Injuries, Feature Set 1
+# Train Score 0.57 / Test Score 0.24 / RMSE 81.05
+# Top 5: pop_dens, race_minority, trav_trans, age_millenial, trav_walk
+# Bottom 5: trav_motorcycle, trav_bike, edu_genz, age_grad, widowed
 rando(df = f1, 
-      city = "LA", 
+      city = "NYC", 
       y_var = "Casualties", 
       n_trees = 500, 
       depth = 5,
-      max_feat = 0.5
+      max_feat = 0.50
       )
 
-# Run random forest model on LA, Total Injuries, Feature Set 2
-rando(df = f2, 
-      city = "LA", 
+# Run random forest model on NYC, Total Injuries, Feature Set 2
+# Train Score 0.51 / Test Score 0.25 / RMSE 80.31
+# Top 5: pop_dens, perc_minority, perc_walk, perc_bs, perc_hsged
+# Bottom 5: perc_mbike, perc_bike, perc_genx, perc_widowed, perc_retiree
+rando(df = f2,
+      city = "NYC", 
       y_var = "Casualties", 
       n_trees = 500, 
       depth = 5,
-      max_feat = 0.5
+      max_feat = 0.50
       )
 
-# Run random forest model on LA, Total Injuries, Feature Set 3
+# Run random forest model on NYC, Total Injuries, Feature Set 3
+# Train Score 0.52 / Test Score 0.20 / RMSE 82.99
+# Top 5: cars_dens, bs_dens, boomber_dens, median_earnings, retiree_dens
+# Bottom 5: mbike_dens, bike_dens, female_dens, trans_dens, millenial_dens
 rando(df = f3,
-      city = "LA", 
+      city = "NYC", 
       y_var = "Casualties", 
       n_trees = 500, 
       depth = 5,
       max_feat = 0.5
+      )
+
+# Run random forest model on NYC, Total Injuries, Feature Set 4 (Ron's new features)
+# Train Score 0.74 / Test Score 0.45 / RMSE 89.04
+# Top 5: road_sumlength, sqml, crime_idx, road_aadt, road_maxlanes
+# Bottom 5: biz_alc, biz_health, biz_lib, fastfood_6mo, fastfood_freq
+rando(df = f4,
+      city = "NYC",
+      y_var = "Casualties",
+      n_trees = 500,
+      depth = 5,
+      max_feat = 0.50
       )
